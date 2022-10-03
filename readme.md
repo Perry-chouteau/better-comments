179ffdc5960c108e7f65377b1b3ad0575ee2dbd8
Chouteau Perry <72015188+Perry-chouteau@users.noreply.github.com>
2022-10-03 23:59:52 +0200

    feat: settings.json, readme.md test.c

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Install 
+
+> https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments
+
+- ext install aaron-bond.better-comments
---
+++ test.c
@@ -0,0 +1,10 @@
+/**
+ * * todo : how to create a template class with a template function.
+ * <> fixme : how to create a template class with a template function.
+
+ * ? help : how to create a template class with a template function. 
+ * ! wrong : how to create a template class with a template function.
+ * $ debug : g++ -std=c++17 -Wall -Wextra -Wpedantic -Werror -g -O0 -o step_two step_two.cpp
+ * /!\ warn : how to create a template class with a template function.
+ * // deprecated : std::map<std::type_index, std::any> _registry;
+ */
